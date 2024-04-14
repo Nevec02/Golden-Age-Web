cb3572758c911602fce4e10774203f9816849a34
Nevec02 <nachocda@gmail.com>
2024-04-14 20:43:36 +0200

    Fixes db conn

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -1,11 +1,9 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
-export const conn = mysql.createConnection({
+export const conn = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-}
-    
-)
+    port: process.env.DB_PORT,
+});
