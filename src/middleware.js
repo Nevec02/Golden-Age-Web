6cdfba1caafef8b61e3c85b4519622c67e3525e7
Nevec02 <nachocda@gmail.com>
2024-04-19 19:20:36 +0200

    Created test middleware

---
+++ src/middleware.js
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+    // TODO: Add user authentication. For test right now /User is private
+    const user = '';
+    if(!user)
+    {
+        return NextResponse.redirect(new URL ('/', request.url));
+    }
+    return NextResponse.next();
+};
+
+export const config = {
+    matcher: ['/user']
+}
